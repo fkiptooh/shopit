189eb39779b8d515e5a331fdb640b8c5dab598b8
fkiptooh <fkiptooh@gmail.com>
2023-03-24 12:00:34 +0300

    Unhadled Promise Rejections

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ require('dotenv').config({ path: './config/config.env'})
 connectDB();
 
 
-app.listen(process.env.PORT, ()=> {
+const server = app.listen(process.env.PORT, ()=> {
     console.log(`Server running on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+})
+
+// Handling the Unhandled promise rejection error
+process.on('unhandledRejection', err=> {
+    console.log(`ERROR: ${err.message}`)
+    console.log(`Shutting down the server due to unhadled promise rejection`)
+    server.close(()=>{
+        process.exit(1)
+    })
 })
