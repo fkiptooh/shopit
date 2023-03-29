f00f5396623eef78449321020b92d5425c34f11e
fkiptooh <fkiptooh@gmail.com>
2023-03-29 12:16:23 +0300

    Handling duplicates and json web token errors in production environment

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -30,6 +30,23 @@ module.exports = (err, req, res, next) => {
             err = new ErrorHandler(message, 400)
         }
 
+        // Handling mongoose duplicate keys
+        if(err.code === 11000){
+            const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+            err = new ErrorHandler(message, 400)
+        }
+        // Json webtoken err
+        if (err.name === 'JsonWebTokenError'){
+            const message = "Json web token is invalid. Try again!!";
+            err = new ErrorHandler(message, 400)
+        }
+
+        // Token has expired
+        if (err.name === 'TokenExpiredError'){
+            const message = "Json web token has expired!";
+            error = new ErrorHandler(message, 400)
+        }
+
         res.status(error.statusCode).json({
             success: false,
             message: err.message || 'Internal server error'
