63bd681e9abf49e04263e51fdb5e796f761d34a0
fkiptooh <fkiptooh@gmail.com>
2023-04-11 09:55:41 +0300

    Created the redux store and the first productReducer

---
+++ frontend/src/constants/productConstants.js
@@ -0,0 +1,12 @@
+// const productConstants = {
+//     ALL_PRODUCTS_REQUEST: 'ALL_PRODUCTS_REQUEST',
+//     ALL_PRODUCTS_SUCCESS: 'ALL_PRODUCTS_SUCCESS',
+//     ALL_PRODUCTS_FAIL: 'ALL_PRODUCTS_FAIL',
+//     CLEAR_ERRORS: 'CLEAR_ERRORS',
+//   };
+  
+//   export default productConstants;
+export const ALL_PRODUCTS_REQUEST = 'ALL_PRODUCTS_REQUEST';
+export const ALL_PRODUCTS_SUCCESS = 'ALL_PRODUCTS_SUCCESS';
+export const ALL_PRODUCTS_FAIL = 'ALL_PRODUCTS_FAIL';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
