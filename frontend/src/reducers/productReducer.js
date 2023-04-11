be34e0117e07f675ef299a0093141326c14eef40
fkiptooh <fkiptooh@gmail.com>
2023-04-11 13:53:11 +0300

    Created a product details reducer and action

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,6 +1,9 @@
 import { ALL_PRODUCTS_SUCCESS, 
          ALL_PRODUCTS_REQUEST, 
          ALL_PRODUCTS_FAIL, 
+         PRODUCT_DETAILS_REQUEST,
+         PRODUCT_DETAILS_SUCCESS,
+         PRODUCT_DETAILS_FAIL,
          CLEAR_ERRORS} from "../constants/productConstants";
 
 export const productReducer = (state = { products: []}, action) => {
@@ -28,5 +31,32 @@ export const productReducer = (state = { products: []}, action) => {
             }
         default:
             return state;
+    }
+}
+
+export const productDetailReducer = (state ={ product: {} }, action)=> {
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case PRODUCT_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                product: action.payload
+            }
+        case PRODUCT_DETAILS_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+        default:
+            return state
     }
 }
